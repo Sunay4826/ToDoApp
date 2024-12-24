8439f316a660f77f6295a7e9f2a41768c693e1c1
sunayrevad2005@gmail.com <Sunay4826>
2024-12-24 22:24:49 +0530

    created frontend and changes in backend

---
+++ vite-project/src/component/createtodo.jsx
@@ -0,0 +1,9 @@
+export function Createtodo(){
+    return(
+        <div>
+            <input type="text" placeholder="Enter your task" /><br />
+            <input type="text" value="about task" /><br />
+            <button>Add a todo</button>
+
+        </div>
+)}
